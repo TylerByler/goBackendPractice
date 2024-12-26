4b0850fcdd7b527f39beed41044594e5749c16a3
TylerByler <tylerbyler203@gmail.com>
2024-12-26 13:07:33 -0500

    Add Files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goBackendWebsite
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for key, value := range r.Form {
+		fmt.Println("key: ", key)
+		fmt.Println("value: ", strings.Join(value, ""))
+	}
+
+	fmt.Fprintf(w, "Hello astaxie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		// create a unique token for every form to prevent duplicate submissions
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("login.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			// validate token
+		} else {
+			// give error if no token
+		}
+		fmt.Println("username length", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
